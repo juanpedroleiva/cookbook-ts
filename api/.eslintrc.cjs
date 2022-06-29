ec7c01b9c3594845d73c89ace7471743e06c5835
Juan Leiva <juan.ferraressoleiva@gmail.com>
2022-06-29 18:35:19 -0300

    ESLint configs to support mocha/chai

--- api/.eslintrc.cjs
+++ api/.eslintrc.cjs
@@ -5,22 +5,25 @@ module.exports = {
         node: true,
     },
     parser: '@typescript-eslint/parser',
-    plugins: ['prettier', 'import', '@typescript-eslint', 'jest'],
+    plugins: ['prettier', 'import', '@typescript-eslint', 'mocha'],
     overrides: [
         {
             files: ['*.ts', '*.tsx'],
-            extends: ['airbnb-typescript/base', 'plugin:@typescript-eslint/eslint-recommended', 'plugin:@typescript-eslint/recommended', 'plugin:jest/recommended', 'prettier'],
+            extends: ['airbnb-typescript/base', 'plugin:@typescript-eslint/eslint-recommended', 'plugin:@typescript-eslint/recommended', 'prettier', 'plugin:mocha/recommended'],
             parserOptions: {
-                project: ['./tsconfig.json'], // Only for TypeScript files
+                sourceType: "module",
+                project: ['./tsconfig.json', './tsconfig.eslint.json'], // Only for TypeScript files
                 tsconfigRootDir: __dirname
             },
             rules: {
                 'max-len': ["error", { "code": 100 }],
                 semi: ['error'],
+                'mocha/no-mocha-arrows': 'off',
                 'no-unused-vars': 'off',
                 '@typescript-eslint/no-unused-vars': 'off',
                 'no-console': 'off',
                 'prettier/prettier': 'off',
+                "import/no-extraneous-dependencies": ["error", {"devDependencies": true}]
             },
         },
     ],
